42783c5cde8bbd6b024bb52fe2f1be49b92d98d3
Gautam  Sapre <gautamsapre@Gautams-MBP.attlocal.net>
2019-09-14 12:33:54 -0500

    ok

--- action.php
+++ action.php
@@ -1,8 +1,8 @@
 <?php
  $path = 'data.txt';
- if (isset($_POST['field1']) && isset($_POST['field2'])) {
+ if (isset($_POST['name']) && isset($_POST['topic'])) {
     $fh = fopen($path,"a+");
-    $string = $_POST['field1'].' - '.$_POST['field2'];
+    $string = $_POST['name'].' - '.$_POST['topix'];
     fwrite($fh,$string); // Write information to the file
     fclose($fh); // Close the file
  }
